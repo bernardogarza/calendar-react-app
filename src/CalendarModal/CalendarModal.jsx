93a1a2ce0ec7bf9be6512ba23dead99da43ed7b8
Bernardo Garza <bernardo.g.landa@gmail.com>
2021-06-02 13:21:55 -0500

    Added modal form

--- src/CalendarModal/CalendarModal.jsx
+++ src/CalendarModal/CalendarModal.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import Modal from 'react-modal';
 
 const customStyles = {
@@ -15,23 +14,63 @@ const customStyles = {
 Modal.setAppElement('#root');
 
 const CalendarModal = () => {
-  const [isModalOpen, setIsModalOpen] = useState(true);
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+  const closeModal = () => {};
 
   return (
     <Modal
-      isOpen={isModalOpen}
+      isOpen={true}
       onRequestClose={closeModal}
       style={customStyles}
       closeTimeoutMS={200}
       className="modal"
       overlayClassName="modal-background"
     >
-      <h1>Hell</h1>
+      <h1> New Event </h1>
       <hr />
-      <span>Hello Again</span>
+      <form className="container">
+        <div className="form-group">
+          <label>Start date and time</label>
+          <input className="form-control" placeholder="Start Date" />
+        </div>
+
+        <div className="form-group">
+          <label>End date and time</label>
+          <input className="form-control" placeholder="End Date" />
+        </div>
+
+        <hr />
+        <div className="form-group">
+          <label>Title and notes</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Event Title"
+            name="title"
+            autoComplete="off"
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            A brief description
+          </small>
+        </div>
+
+        <div className="form-group">
+          <textarea
+            type="text"
+            className="form-control"
+            placeholder="Notes"
+            rows="5"
+            name="notes"
+          ></textarea>
+          <small id="emailHelp" className="form-text text-muted">
+            Additional info...
+          </small>
+        </div>
+
+        <button type="submit" className="btn btn-outline-primary btn-block">
+          <i className="far fa-save"></i>
+          <span> Save</span>
+        </button>
+      </form>
     </Modal>
   );
 };
