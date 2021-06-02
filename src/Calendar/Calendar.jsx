fc1d1e01c0a4c7d6ec604924f731b21923cf4984
Bernardo Garza <bernardo.g.landa@gmail.com>
2021-06-02 12:02:10 -0500

    Added calendar

--- src/Calendar/Calendar.jsx
+++ src/Calendar/Calendar.jsx
@@ -1,9 +1,25 @@
+import moment from 'moment';
+import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar';
+
 import Navbar from '../Navbar/Navbar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+  {
+    title: 'Birthday',
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgcolor: '#fafafa',
+  },
+];
 
 const Calendar = () => {
   return (
     <div>
       <Navbar />
+      <BigCalendar localizer={localizer} events={events} startAccessor="start" endAccessor="end" />
     </div>
   );
 };
