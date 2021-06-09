3d7522f12589f466ea752e811bb7dc8d11d57f78
Bernardo Garza <bernardo.g.landa@gmail.com>
2021-06-09 10:53:50 -0500

    Created custom hook and setted up the login form

--- src/components/LoginScreen/LoginScreen.jsx
+++ src/components/LoginScreen/LoginScreen.jsx
@@ -1,19 +1,51 @@
+import { useForm } from '../../hooks/useForm';
 import './LoginScreen.css';
 
 const LoginScreen = () => {
+  const [formLoginValues, handleLoginInputChange] = useForm({
+    loginEmail: 'bernardo@mail.com',
+    loginPassword: '123456',
+  });
+
+  const { loginEmail, loginPassword } = formLoginValues;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(formLoginValues);
+  };
+
   return (
     <div>
       <div className="container login-container">
         <div className="row">
           <div className="col-md-6 login-form-1">
             <h3>Log In</h3>
-            <form>
+
+            <form onSubmit={handleLogin}>
               <div className="form-group">
-                <input type="text" className="form-control" placeholder="Correo" />
+                <label>Email</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Email"
+                  name="loginEmail"
+                  value={loginEmail}
+                  onChange={handleLoginInputChange}
+                />
               </div>
+
               <div className="form-group">
-                <input type="password" className="form-control" placeholder="Contraseña" />
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Password"
+                  name="loginPassword"
+                  value={loginPassword}
+                  onChange={handleLoginInputChange}
+                />
               </div>
+
               <div className="form-group">
                 <input type="submit" className="btnSubmit" value="Login" />
               </div>
@@ -22,22 +54,30 @@ const LoginScreen = () => {
 
           <div className="col-md-6 login-form-2">
             <h3>Sign Up</h3>
+
             <form>
               <div className="form-group">
-                <input type="text" className="form-control" placeholder="Nombre" />
-              </div>
-              <div className="form-group">
-                <input type="email" className="form-control" placeholder="Correo" />
-              </div>
-              <div className="form-group">
-                <input type="password" className="form-control" placeholder="Contraseña" />
+                <label className="signup-label">Name</label>
+                <input type="text" className="form-control" placeholder="Name" />
               </div>
 
               <div className="form-group">
+                <label className="signup-label">Email</label>
+                <input type="email" className="form-control" placeholder="Email" />
+              </div>
+
+              <div className="form-group">
+                <label className="signup-label">Password</label>
+                <input type="password" className="form-control" placeholder="Password" />
+              </div>
+
+              <div className="form-group">
+                <label className="signup-label">Password Confirmation</label>
+
                 <input
                   type="password"
                   className="form-control"
-                  placeholder="Repita la contraseña"
+                  placeholder="Password Confirmation"
                 />
               </div>
 
