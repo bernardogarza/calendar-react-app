ccdc20f4316b75e126e4739e980ed2d7ed2f786a
Bernardo Garza <bernardo.g.landa@gmail.com>
2021-06-10 13:05:13 -0500

    Fixed issue where redux dev tools showed in production

--- src/store/store.js
+++ src/store/store.js
@@ -3,8 +3,8 @@ import thunk from 'redux-thunk';
 import { rootReducer } from '../reducers/rootReducer';
 
 const composeEnhancers =
-  (process.env.NODE_ENV === 'development'
+  process.env.NODE_ENV === 'development'
     ? typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null) || compose;
+    : null || compose;
 
 export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
