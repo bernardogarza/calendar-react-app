e4daa0758779bb54e8190209116ce2cbcac3904e
Bernardo Garza <bernardo.g.landa@gmail.com>
2021-06-29 15:11:02 -0500

    Created first test to check types

---
+++ src/__tests__/types/types.test.js
@@ -0,0 +1,25 @@
+import { types } from '../../types/types';
+
+describe('Types tests', () => {
+  it('should return the same types', () => {
+    expect(types).toEqual({
+      uiOpenModal: '[ui] Open modal',
+      uiCloseModal: '[ui] Close modal',
+
+      eventStartAddNew: '[event] Start add new',
+      eventSetActive: '[event] Set active',
+      eventAddNew: '[event] Add new',
+      eventCleanActive: '[event] Clean active',
+      eventEdit: '[event] Update event',
+      eventDelete: '[event] Delete event',
+      eventLoaded: '[event] Events loaded',
+
+      authCheckingFinish: '[auth] Finish checking login state',
+      authStartLogin: '[auth] Start login',
+      authLogin: '[auth] Login',
+      authStartSignup: '[auth] Sign up',
+      authStartTokenRenewal: '[auth] Start token renewal',
+      authLogout: '[auth] Logout',
+    });
+  });
+});
